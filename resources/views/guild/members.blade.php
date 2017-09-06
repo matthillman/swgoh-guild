ded66eda49f8f7ed17801d010e76b6305d7e08ed
Matthew Hillman <mhillman@bomgar.com>
2017-09-06 11:12:44 -0500

    Propper title

--- resources/views/guild/members.blade.php
+++ resources/views/guild/members.blade.php
@@ -4,7 +4,7 @@
 
 <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
     <div class="mdl-card__title">
-        <h2 class="mdl-card__title-text fill center">Today's Payouts</h2>
+        <h2 class="mdl-card__title-text fill center">Guild Characters</h2>
     </div>
 
     <div class="mdl-card__supporting-text flex-vertical-centered wide">
