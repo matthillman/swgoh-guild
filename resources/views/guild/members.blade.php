d9b501ad5fcc3d884056c5a7c379ef98b9970a28
Matthew Hillman <mhillman@bomgar.com>
2017-09-06 11:12:09 -0500

    Better layout

--- resources/views/guild/members.blade.php
+++ resources/views/guild/members.blade.php
@@ -1,26 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<table>
-	<thead>
-		<tr>
-			<th>Character</th>
-			<th>Member</th>
-			<th>Rarity</th>
-			<th>Level</th>
-			<th>Gear</th>
-		</tr>
-	</thead>
-	<tbody>
-		@foreach ($guild->characters->sortBy('name') as $character)
-		<tr>
-			<td>{{ $character->name }}</td>
-			<td>{{ $character->member->name }}</td>
-			<td>{{ $character->rarity }}</td>
-			<td>{{ $character->level }}</td>
-			<td>{{ $character->gear_level }}</td>
-		</tr>
-		@endforeach
-	</tbody>
-</table>
+
+<div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
+    <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text fill center">Today's Payouts</h2>
+    </div>
+
+    <div class="mdl-card__supporting-text flex-vertical-centered wide">
+		<table>
+			<thead>
+				<tr>
+					<th>Character</th>
+					<th>Member</th>
+					<th>Rarity</th>
+					<th>Level</th>
+					<th>Gear</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ($guild->characters->sortBy('name') as $character)
+				<tr>
+					<td>{{ $character->name }}</td>
+					<td>{{ $character->member->name }}</td>
+					<td>{{ $character->rarity }}</td>
+					<td>{{ $character->level }}</td>
+					<td>{{ $character->gear_level }}</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+</div>
 @endsection
