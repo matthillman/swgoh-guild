c4ea49d95c8f52550bf20a2dd3f121d20f9f312e
Matthew Hillman <mhillman@bomgar.com>
2017-09-06 15:53:22 -0500

    Testing vue

--- resources/views/guild/members.blade.php
+++ resources/views/guild/members.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+<div id="#app"></div>
+
 <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
     <div class="mdl-card__title">
         <h2 class="mdl-card__title-text fill center">Guild Characters</h2>
