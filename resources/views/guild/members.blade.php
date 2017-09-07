60640c1ce5660da64dc205d405863b1d91bd1633
Matthew Hillman <mhillman@bomgar.com>
2017-09-07 11:11:04 -0500

    Delete the static table

--- resources/views/guild/members.blade.php
+++ resources/views/guild/members.blade.php
@@ -15,28 +15,6 @@
 				v-bind:columns="memberColumns"
 			></example>
 		</div>
-		<table>
-			<thead>
-				<tr>
-					<th>Character</th>
-					<th>Member</th>
-					<th>Rarity</th>
-					<th>Level</th>
-					<th>Gear</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach ($guild->characters->sortBy('name') as $character)
-				<tr>
-					<td>{{ $character->name }}</td>
-					<td>{{ $character->member->name }}</td>
-					<td>{{ $character->rarity }}</td>
-					<td>{{ $character->level }}</td>
-					<td>{{ $character->gear_level }}</td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
 	</div>
 </div>
 @endsection
