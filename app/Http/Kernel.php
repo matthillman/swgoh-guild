0b0477b079d48cc5e754c58cdfa9c3546e4b4c5b
Matt <m@hilllman.me>
2017-09-08 20:23:27 +0000

    Varnish

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+	'cacheable' => \Spatie\Varnish\Middleware\CacheWithVarnish::class,
     ];
 }
