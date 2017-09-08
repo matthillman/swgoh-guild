de78c1503493bdc889271fede81c6e610700aa75
Matthew Hillman <mhillman@bomgar.com>
2017-09-08 15:24:21 -0500

    Cache routes

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ Route::get('/', function () {
 });
 
 Route::resource('guild', '\App\Http\Controllers\GuildController');
-Route::get('guild/{id}/members', '\App\Http\Controllers\GuildController@members');
-Route::get('guild/{id}/characters', '\App\Http\Controllers\GuildController@characters');
+
+Route::group(['middleware' => 'cacheable'], function () {
+    Route::get('guild/{id}/members', '\App\Http\Controllers\GuildController@members');
+    Route::get('guild/{id}/characters', '\App\Http\Controllers\GuildController@characters');
+ });
